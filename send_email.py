c7cb782f7b443f3574a8fbe0d379e668710042de
MattMcFahn <44867810+MattMcFahn@users.noreply.github.com>
2020-01-18 13:29:10 +0000

    Privacy update
    Configured for local and production deployment to locate appropriate config variables

--- send_email.py
+++ send_email.py
@@ -1,5 +1,29 @@
 import smtplib
 import config
+import os
+
+def email():
+    """
+    Retreive email
+    """
+    try:
+        from config import emails
+        from_email_add = emails['gmail']
+    except ImportError as e:
+        from_email_add = os.environ.get['GMAIL']
+    return from_email_add
+
+
+def email_password():
+    """
+    Retreive password based on os
+    """
+    try:
+        from config import passwords
+        gmail_password = passwords['gmail']
+    except ImportError as e:
+        gmail_password = os.environ.get['GMAIL_PASSWORD']
+    return gmail_password
 
 def email_comments(email_target, comments):
     """
@@ -8,19 +32,25 @@ def email_comments(email_target, comments):
     noted, and giving them a copy of their submission.
     """
     # NOTE - I should really reformat this as layout is awful
-    
+
     smtpObj = smtplib.SMTP('smtp.gmail.com', 587) # Connect to gmail stmp provider, 'smtp.gmail.com'
     smtpObj.ehlo() # Say "hello" to the server
     smtpObj.starttls() # Connect to port 587 (TLS encryption)
-    secret_password = config.passwords['gmail']
-    smtpObj.login(config.emails['gmail'], secret_password) #Log in to access email
-    #Send the mail
-    smtpObj.sendmail(from_addr=config.emails['gmail'],
-                     to_addrs = email_target,
-                     msg = '''Subject: Submission to my webpage\n
-                     Hi! \n\n
-                     Thank you for submitting a message on my webpage. \n
-                     I will try and get back to you.\n\n\n
 
-                     --- Copy of submission ---\n\n
-                     Comments: '{}' '''.format(comments))
+    from_addr = email()
+    secret_password = email_password()
+    smtpObj.login(config.emails['gmail'], secret_password) #Log in to access email
+    # - Write message
+    msg = '''Subject: Submission to my webpage\n
+             Hi! \n\n
+             Thank you for submitting a message on my webpage. \n
+             I will try and get back to you.\n\n\n
+
+             --- Copy of submission ---\n\n
+             Comments: '{}' '''.format(comments)
+    # - Write message
+
+    #Send the mail
+    smtpObj.sendmail(from_addr=from_addr,
+                     to_addrs = email_target,
+                     msg = msg)
